46d03c5e3f4e0b9129d45574f3c93246d6fd0434
Rafay575 <111084742+Rafay575@users.noreply.github.com>
2024-06-08 02:15:54 +0500

    latest

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,14 +6,7 @@ import Footer from './Components/Footer';
 import Home from './Page/Home';
 
 function App() {
-  const divStyle = {
-    backgroundImage: `url(${process.env.PUBLIC_URL + "/images/Poster.jpg"})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'top',
-    width: '100%',
-    minHeight: '100vh',
-  };
-
+ 
   return (
     <>
       <div >
---
+++ frontend/src/Components/Video.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const ImageCarousel = () => {
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }
+    ]
+  };
+
+  const reverseSettings = {
+   ...settings,
+    rtl: true, 
+  };
+
+  return (
+    <div className="max-w-7xl mx-auto mt-8 gap-4">
+      <Slider {...settings}>
+        <div>
+          <img src="/images/bg-1.jpg" alt="Image 1" className="w-full h-auto" />
+        </div>
+        <div>
+          <img src="/images/bg-1.jpg" alt="Image 2" className="w-full h-auto" />
+        </div>
+        <div>
+          <img src="/images/bg-1.jpg" alt="Image 3" className="w-full h-auto" />
+        </div>
+        <div>
+          <img src="/images/bg-1.jpg" alt="Image 4" className="w-full h-auto" />
+        </div>
+        <div>
+          <img src="/images/bg-1.jpg" alt="Image 5" className="w-full h-auto" />
+        </div>
+        <div>
+          <img src="/images/bg-1.jpg" alt="Image 6" className="w-full h-auto" />
+        </div>
+        <div>
+          <img src="/images/bg-1.jpg" alt="Image 7" className="w-full h-auto" />
+        </div>
+        <div>
+          <img src="/images/bg-1.jpg" alt="Image 8" className="w-full h-auto" />
+        </div>
+      </Slider>
+      <Slider {...reverseSettings}>
+        <div>
+          <img src="/images/bg-2.jpg" alt="Image 9" className="w-full h-auto" />
+        </div>
+        <div>
+          <img src="/images/bg-2.jpg" alt="Image 10" className="w-full h-auto" />
+        </div>
+        <div>
+          <img src="/images/bg-2.jpg" alt="Image 11" className="w-full h-auto" />
+        </div>
+        <div>
+          <img src="/images/bg-2.jpg" alt="Image 12" className="w-full h-auto" />
+        </div>
+        <div>
+          <img src="/images/bg-2.jpg" alt="Image 13" className="w-full h-auto" />
+        </div>
+        <div>
+          <img src="/images/bg-2.jpg" alt="Image 14" className="w-full h-auto" />
+        </div>
+        <div>
+          <img src="/images/bg-2.jpg" alt="Image 15" className="w-full h-auto" />
+        </div>
+        <div>
+          <img src="/images/bg-2.jpg" alt="Image 16" className="w-full h-auto" />
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+export default ImageCarousel;
